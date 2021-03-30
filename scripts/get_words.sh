e30b26eef4742f3ee005d8d894f2478821c75f53
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2021-03-30 01:49:01 -0400

    get_words.sh

--- scripts/get_words.sh
+++ scripts/get_words.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cat /usr/share/dict/words > ../words.txt
+# Retrieve giant word list from garden-variety Unix install
+if [ -e /usr/share/dict/words ]
+then
+    cat /usr/share/dict/words > ../words.txt
+    echo "Success! words.txt created in root of project"
+else
+    echo "/usr/share/dict/words not found"
+fi
+
