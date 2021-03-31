81e82d2bc12c30b367370b9c6480529ac07ecdc5
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2021-03-31 02:47:29 -0400

    Finish get_words.sh

--- scripts/get_words.sh
+++ scripts/get_words.sh
@@ -4,11 +4,11 @@
 if [ -e /usr/share/dict/words ]
 then
     DESTINATION="$( cd "$( dirname "$0" )" && cd .. && pwd )"
-    if [ -e $DESTINATION/words.txt ]
-    then    
-        echo "words.txt already exists in $DESTINATION/"
+    if [ ! -e $DESTINATION/words.txt ]
+    then
+        cat /usr/share/dict/words > $DESTINATION/words.txt
     else
-        :
+        echo "words.txt already exists in $DESTINATION"
     fi
 else
     echo "/usr/share/dict/words not found"
