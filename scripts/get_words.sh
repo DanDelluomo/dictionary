5c0b0df61ff2a21427caece0d6e67eedf3f148d4
Daniel Edward Dell'uomo <dan@MacBook-Pro.local>
2021-03-31 02:31:53 -0400

    Develop bash script: get_words.sh

--- scripts/get_words.sh
+++ scripts/get_words.sh
@@ -3,9 +3,18 @@
 # Retrieve giant word list from garden-variety Unix install
 if [ -e /usr/share/dict/words ]
 then
-    cat /usr/share/dict/words > ../words.txt
-    echo "Success! words.txt created in root of project"
+    DESTINATION="$( cd "$( dirname "$0" )" && cd .. && pwd )"
+    if [ -e $DESTINATION/words.txt ]
+    then    
+        echo "words.txt already exists in $DESTINATION/"
+    else
+        :
+    fi
 else
     echo "/usr/share/dict/words not found"
 fi
+
+
+
+
 
