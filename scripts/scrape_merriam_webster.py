ebefe32d44db179f3b8a413ff59e983d5cd5aab5
Daniel Edward Dell'uomo <dan@MacBook-Pro.lan>
2021-05-20 04:49:01 -0400

    Add build_dictionary.py

--- scripts/scrape_merriam_webster.py
+++ scripts/scrape_merriam_webster.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup as bs
 
 base_url = "https://www.merriam-webster.com/dictionary/"
 
-def merriam_webster_def(word):
+def merriam_webster_def(word: str) -> str:
     """Scrape a word's definition on merriam-webster.com"""
     word_url = base_url + word
-    r = requests.get(word_url)
-    soup = bs(r.content, 'lxml')
+    response = requests.get(word_url)
+    soup = bs(response.content, 'lxml')
     # print(soup.select_one('.mw_t_bc').next_sibling.strip())
     return soup.select_one('.mw_t_bc').next_sibling.strip()
