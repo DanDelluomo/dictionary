1c5121507fd7e92bd63c40476edd4213e8043391
Daniel Edward Dell'uomo <dan@MacBook-Pro.lan>
2021-06-04 22:03:48 -0400

    commit for master merge

--- scripts/scrape_merriam_webster.py
+++ scripts/scrape_merriam_webster.py
@@ -8,10 +8,13 @@ from bs4 import BeautifulSoup as bs
 base_url = "https://www.merriam-webster.com/dictionary/"
 
 def merriam_webster_def(word: str) -> str:
-    """Scrape a word's definition on merriam-webster.com"""
+    """
+    Scrape a word's definition on merriam-webster.com
+    
+    Returns scraped definition.
+    
+    """
     word_url = base_url + word
     response = requests.get(word_url)
     soup = bs(response.content, 'lxml')
-    # print(soup.select_one('.mw_t_bc').next_sibling.strip())
-    return soup.select_one('.mw_t_bc').next_sibling.strip()
-    print(1)
+    return soup.select_one('.dtText').get_text()
