53f3efd20045b6cb9f16739fc20180984682c360
Daniel Edward Dell'uomo <dan@MacBook-Pro.lan>
2021-06-04 21:46:42 -0400

    clean folders

--- scripts/build_dictionary.py
+++ scripts/build_dictionary.py
@@ -16,16 +16,16 @@ with open('../words.txt', 'r') as f:
 words_dict = {}
 for word in words_list:
     # Slight throttle to avoid overloading merriam-webster.com 
-    time.sleep(0.01)
+    # time.sleep(0.01)
     try:
         definition = merriam_webster_def(word)
         words_dict[word] = definition
-        with open('../logs/defs', 'a') as f:
-            f.write(f"{word} !@#$%^& {definition}\n")
+        with open('../logs/defs.02', 'a') as f:
+            f.write(f"{word} !/!/! {definition}\n")
     except BaseException as e:
-        with open('../logs/failures', 'a') as f:
+        with open('../logs/failures.02', 'a') as f:
             f.write(f"Couldn't define {word} due to: {e}\n")
 
-with open('dict.pickle', 'wb') as handle:
+with open('dict.pickle.02', 'wb') as handle:
     pickle.dump(words_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
