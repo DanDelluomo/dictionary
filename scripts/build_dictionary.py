70a995bb6cb6cfc838b4cfce0ff4c10a052aa36b
Daniel Edward Dell'uomo <dan@MacBook-Pro.lan>
2021-06-04 22:42:12 -0400

    Figure progress by # of lines in defs file

--- scripts/build_dictionary.py
+++ scripts/build_dictionary.py
@@ -13,17 +13,23 @@ from scrape_merriam_webster import merriam_webster_def
 with open('../words.txt', 'r') as f:
    words_list = f.read().split()
 
+# Figure out overall progress 
+with open('logs/defs') as f:
+    number_defs = len(f.readlines())
+
+words_list = words_list[number_defs:]
+
 words_dict = {}
 for word in words_list:
     # Slight throttle to avoid overloading merriam-webster.com 
-    # time.sleep(0.01)
+    time.sleep(0.01)
     try:
         definition = merriam_webster_def(word)
         words_dict[word] = definition
-        with open('../logs/defs.02', 'a') as f:
+        with open('../logs/defs', 'a') as f:
             f.write(f"{word} !/!/! {definition}\n")
     except BaseException as e:
-        with open('../logs/failures.02', 'a') as f:
+        with open('../logs/failures', 'a') as f:
             f.write(f"Couldn't define {word} due to: {e}\n")
 
 with open('dict.pickle.02', 'wb') as handle:
