f4f1505e8f678992ed5296cd499e4f1dcf2cdc89
Daniel Edward Dell'uomo <dan@MacBook-Pro.lan>
2021-06-16 12:48:36 -0400

    change to pathlib

--- scripts/build_dictionary.py
+++ scripts/build_dictionary.py
@@ -5,16 +5,22 @@
 # Import Standard Libary Packages
 import pickle
 import time
+import pathlib
+
+parent_path = pathlib.Path(__file__).absolute().parents[1] 
+words_path = parent_path / "tools" / "words.txt"
+defs_path = parent_path / "defs_test"
+failures_path = parent_path / "logs" / "failures"
 
 # Import project modules
 from scrape_merriam_webster import merriam_webster_def
 
 # Retrieve massive Unix system word list
-with open('../words.txt', 'r') as f:
+with open(words_path, 'r') as f:
    words_list = f.read().split()
 
 # Figure out last defined word  
-with open('../logs/defs') as f:
+with open(defs_path) as f:
     new_index = 0
     whole_file = f.readlines()
     if len(whole_file) > 2:
@@ -31,12 +37,12 @@ for word in words_list:
     try:
         definition = merriam_webster_def(word)
         words_dict[word] = definition
-        with open('../logs/defs', 'a') as f:
+        with open(defs_path, 'a') as f:
             f.write(f"{word} {definition}\n")
     except BaseException as e:
-        with open('../logs/failures', 'a') as f:
+        with open(failures_path, 'a') as f:
             f.write(f"Couldn't define {word} due to: {e}\n")
 
-with open('dict.pickle', 'wb') as handle:
-    pickle.dump(words_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# with open('dict.pickle', 'wb') as handle:
+#     pickle.dump(words_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
