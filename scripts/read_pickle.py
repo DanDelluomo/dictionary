7c080a0c7d8b03f5f0c633d662a96382845b295e
Daniel Edward Dell'uomo <dan@MacBook-Pro.lan>
2021-06-19 00:26:33 -0400

    Save work

---
+++ scripts/read_pickle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Import Standard Libary Packages
+import pathlib
+import pickle
+
+def get_dict():
+    with open('dict.pickle', 'rb') as pickle_dict:
+        return pickle.load(pickle_dict)
+
